9b00c0bdfc4fc134d7d93f692b797eabe16047f7
Paula Ritter <manritter@uos.de>
2021-01-29 16:02:39 +0100

    remove time.sleep and add print for env variable

--- monitoring-script.py
+++ monitoring-script.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup    # to parse what we download
 import time    # to add delay between runs
 import smtplib    # to allow us to email
 
+print(os.environ['ALREADYSENT'])
 
 url = "https://www.carved.com/collections/custom-block-live-edges-all"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -11,7 +12,7 @@ response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text,  "lxml")
     
 if str(soup).find("card-product") > -1:
-    if os.environ['ALREADYSENT'] is '0':
+    if os.environ['ALREADYSENT'] == '0':
         msg = 'Subject: script found new live edge case!!'
         fromaddr = 'codingpaula@gmail.com'
         toaddrs = ['paula.ritter@protonmail.com']
@@ -32,5 +33,3 @@ if str(soup).find("card-product") > -1:
 else:
     os.environ['ALREADYSENT'] = '0'
     print('no live edge case was found')
-    
-time.sleep(300)
